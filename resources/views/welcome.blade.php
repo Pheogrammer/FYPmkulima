a89ad25c107ea2546d0e485b611d17fd5b13e2b5
hagtec <hagai.medrick2@gmail.com>
2023-05-31 01:35:25 -1200

    frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
                                     </li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#"
-                                            data-bs-toggle="dropdown"Hali ya hewa</a>
+                                            data-bs-toggle="dropdown">Hali ya hewa</a>
                                     </li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#"
