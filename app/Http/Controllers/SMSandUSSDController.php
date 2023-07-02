f0348f581a203c9bc20ee89dd2fc8054aa500db6
Alfeo Raymond <ngelime07@gmail.com>
2023-07-02 14:33:45 +0300

    add AT

--- app/Http/Controllers/SMSandUSSDController.php
+++ app/Http/Controllers/SMSandUSSDController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use AfricasTalking\SDK\AfricasTalking;
 
 class SMSandUSSDController extends Controller
 {
     //
+    public function sendSMS()
+    {
+        $username = 'YOUR_USERNAME';
+        $apiKey   = 'YOUR_API_KEY';
+        $AT       = new AfricasTalking($username, $apiKey);
+
+
+        $sms      = $AT->sms();
+
+        $result   = $sms->send([
+            'to'      => '+2XXYYYOOO',
+            'message' => 'Hello World!'
+        ]);
+
+        print_r($result);
+    }
 }
