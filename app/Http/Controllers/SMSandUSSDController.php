bc217cbc5e4faded0f5988069ad0e18886d059d4
Pheogrammer <ngelime07@gmail.com>
2023-08-01 23:36:56 +0300

    commit comments removed

--- app/Http/Controllers/SMSandUSSDController.php
+++ app/Http/Controllers/SMSandUSSDController.php
@@ -14,33 +14,26 @@ class SMSandUSSDController extends Controller
 
     public function ussd(Request $request)
     {
-        // Extract variables from the request
         $sessionId = $request->input('sessionId');
         $serviceCode = $request->input('serviceCode');
         $phoneNumber = $request->input('phoneNumber');
         $text = $request->input('text');
 
-        // Initialize the response variable
-        $response = 'CON Hello!';
+        $response = 'END Samahani, hatujaelewa uchaguzi wako!';
 
-        // ... (your existing code)
 
         if ($text == '') {
-            // This is the first request
             $response = "CON Habari, Karibu.\n";
             $response .= "Je, ungependa kupata huduma gani?\n";
             $response .= "1. Kujua bei za mazao\n";
             $response .= "2. Kujiunga na huduma ya ujumbe mfupi kupata dondoo za kilimo";
         } else {
-            // Explode the text to get the different parts
             $parts = explode('*', $text);
 
-            // Get the first part of the user input
             $mainMenuOption = $parts[0];
 
             if ($mainMenuOption === '1') {
                 if (count($parts) === 1) {
-                    // Show zones
                     $response = "CON Je, upo katika kanda ipi?\n";
                     $zones = Zone::all();
                     if ($zones->count() > 0) {
@@ -51,48 +44,37 @@ class SMSandUSSDController extends Controller
                         $response .= "Hakuna kanda kwa sasa.\n";
                     }
                 } elseif (count($parts) === 2) {
-                    // Show regions for the selected zone
                     $selectedZoneIndex = intval($parts[1]);
                     $regions = Region::where('zoneID', $selectedZoneIndex)->get();
                     $response = "CON Je, upo katika mkoa upi?\n";
                     if ($regions->count() > 0) {
                         foreach ($regions as $region) {
-                            $response .= $region->id . '. ' . $region->name . "\n"; // Display the region ID instead of SN
+                            $response .= $region->id . '. ' . $region->name . "\n";
                         }
                     } else {
                         $response .= "Hakuna mikoa iliyosajiliwa katika kanda uliyochagua.\n";
                     }
                 } elseif (count($parts) === 3) {
-                    // Show crops for the selected region
-                    $selectedRegionID = intval($parts[2]); // Use the selected region ID from the user input
-                    $region = Region::find($selectedRegionID); // Fetch the region directly using the ID
+                    $selectedRegionID = intval($parts[2]);
+                    $region = Region::find($selectedRegionID);
                     if (!$region) {
-                        // Invalid region ID
                         $response = "END Mkoa uliochagua haupatikani. Tafadhali jaribu tena.";
                     } else {
                         $cropPrices = Price::where('regionID', $selectedRegionID)->with('crop')->get();
 
-                        // Check if there are crops to display
                         if ($cropPrices->count() > 0) {
-                            // Calculate the total number of crops
                             $totalCrops = $cropPrices->count();
 
-                            // Calculate the current offset based on user input
                             $currentOffset = intval(end($parts)) ?: intval($parts[3] ?? 0);
 
-                            // Check if the user responded with 0 to fetch the next group of crops
                             if ($currentOffset === 0) {
-                                // If the user entered 0, update the offset to fetch the next group of crops
-                                $currentOffset = intval($parts[4] ?? 1); // Use the next part as the new offset
+                                $currentOffset = intval($parts[4] ?? 1);
                             }
 
-                            // Calculate the next offset
                             $nextOffset = $currentOffset + 5;
 
-                            // Get the current group of crops to display
                             $currentCropPrices = $cropPrices->slice($currentOffset - 1, 5);
 
-                            // Prepare the response
                             $region = Region::find($selectedRegionID);
                             $response = "CON Bei za mazao " . $region['name'] . ":\n";
 
@@ -101,47 +83,34 @@ class SMSandUSSDController extends Controller
                                 $response .= $displayNumber . '. ' . $price->crop->name . " Tsh " . number_format($price->maxprice) . " \n";
                             }
 
-                            // Check if there are more crops to display
                             if ($nextOffset <= $totalCrops) {
-                                // If there are more crops, show the option to see the next list
                                 $response .= "Bonyeza 0 kuona mazao mengine.\n";
                             } else {
-                                // If there are no more crops, show the option to go back to the main menu or exit
                                 $response .= "Bonyeza 100 kurudi kwenye menyu kuu.\n";
                             }
                         }
                     }
                 } elseif ((count($parts) === 4 && intval(end($parts)) === 0)) {
-                    // Show crops for the selected region
-                    $selectedRegionID = intval($parts[2]); // Use the selected region ID from the user input
-                    $region = Region::find($selectedRegionID); // Fetch the region directly using the ID
+                    $selectedRegionID = intval($parts[2]);
+                    $region = Region::find($selectedRegionID);
                     if (!$region) {
-                        // Invalid region ID
                         $response = "END Mkoa uliochagua haupatikani. Tafadhali jaribu tena.";
                     } else {
                         $cropPrices = Price::where('regionID', $selectedRegionID)->with('crop')->get();
 
-                        // Check if there are crops to display
                         if ($cropPrices->count() > 0) {
-                            // Calculate the total number of crops
                             $totalCrops = $cropPrices->count();
 
-                            // Calculate the current offset based on user input
                             $currentOffset = intval(end($parts)) ?: intval($parts[3] ?? 0);
 
-                            // Check if the user responded with 0 to fetch the next group of crops
                             if ($currentOffset === 0) {
-                                // If the user entered 0, update the offset to fetch the next group of crops
-                                $currentOffset = intval($parts[4] ?? 1); // Use the next part as the new offset
+                                $currentOffset = intval($parts[4] ?? 1);
                             }
 
-                            // Calculate the next offset
                             $nextOffset = $currentOffset + 5;
 
-                            // Get the current group of crops to display
                             $currentCropPrices = $cropPrices->slice($currentOffset - 1, 5);
 
-                            // Prepare the response
                             $region = Region::find($selectedRegionID);
                             $response = "CON Bei za mazao " . $region['name'] . ":\n";
 
@@ -150,23 +119,18 @@ class SMSandUSSDController extends Controller
                                 $response .= $displayNumber . '. ' . $price->crop->name . " Tsh " . number_format($price->maxprice) . " \n";
                             }
 
-                            // Check if there are more crops to display
                             if ($nextOffset <= $totalCrops) {
-                                // If there are more crops, show the option to see the next list
                                 $response .= "Bonyeza 0 kuona mazao mengine.\n";
                             } else {
-                                // If there are no more crops, show the option to go back to the main menu or exit
                                 $response .= "Bonyeza 100 kurudi kwenye menyu kuu.\n";
                             }
                         }
                     }
                 } else {
-                    // Invalid input at this level
                     $response = "END Taarifa uliyoomba haipo, tafadhali jaribu tena";
                 }
             }
 
-            // ... (your existing code)
         }
 
         // Send the response back to the API
